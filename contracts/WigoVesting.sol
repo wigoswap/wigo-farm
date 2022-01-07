943c302e5949715e5d9b65778ee11ad457ab3fee
WigoSwap <wearewigo@protonmail.com>
2022-01-07 00:20:03 +0000

    Refactor WigoVesting

--- contracts/WigoVesting.sol
+++ contracts/WigoVesting.sol
@@ -1,78 +1,68 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.6.12;
 
-import "./OpenZeppelin/token/ERC20/IERC20.sol";
-import "./OpenZeppelin/token/ERC20/TokenTimeLock.sol";
 import "./OpenZeppelin/token/ERC20/SafeERC20.sol";
-import "./OpenZeppelin/math/SafeMath.sol";
 
-// To see openzepplin's audits goto: https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/audit
+/**
+ * @dev A token holder contract that will allow a beneficiary to extract the
+ * tokens after a given release time.
+ *
+ * Useful for simple vesting schedules like "advisors get all of their tokens
+ * after 1 year".
+ */
 contract WigoVesting {
-    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    IERC20 public wigo;
-    IERC20 public wigoLP;
-    address public coreTeam;
-    address public seedFunder1;
-    address public seedFunder2;
+    // ERC20 basic token contract being held
+    IERC20 private immutable _token;
 
-    TokenTimelock[] public Locks;
+    // beneficiary of tokens after they are released
+    address private immutable _beneficiary;
 
-    event Release(uint256 lockNumber, address indexed beneficiary);
+    // timestamp when token release is enabled
+    uint256 private immutable _releaseTime;
 
     constructor(
-        IERC20 _wigo,
-        IERC20 _wigoLP,
-        address _coreTeam,
-        address _seedFunder1,
-        address _seedFunder2
+        IERC20 token_,
+        address beneficiary_,
+        uint256 releaseTime_
     ) public {
-        wigo = _wigo;
-        wigoLP = _wigoLP;
-        coreTeam = _coreTeam;
-        seedFunder1 = _seedFunder1;
-        seedFunder2 = _seedFunder2;
-        uint256 currentTime = block.timestamp;
-
-        // Seed Funder 1 Vesting Contracts
-        createLock(wigo, seedFunder1, currentTime.add(60 days)); // Lock 0
-        createLock(wigo, seedFunder1, currentTime.add(120 days)); // Lock 1
-        createLock(wigo, seedFunder1, currentTime.add(180 days)); // Lock 2
-        createLock(wigo, seedFunder1, currentTime.add(240 days)); // Lock 3
-
-        // Seed Funder 2 Vesting Contracts
-        createLock(wigo, seedFunder2, currentTime.add(60 days)); // Lock 4
-        createLock(wigo, seedFunder2, currentTime.add(120 days)); // Lock 5
-        createLock(wigo, seedFunder2, currentTime.add(180 days)); // Lock 6
-        createLock(wigo, seedFunder2, currentTime.add(240 days)); // Lock 7
-
-        // Core Team Vesting Contract
-        createLock(wigoLP, coreTeam, currentTime.add(365 days)); // Lock 8
+        require(releaseTime_ > block.timestamp, "WigoVesting: release time is before current time");
+        _token = token_;
+        _beneficiary = beneficiary_;
+        _releaseTime = releaseTime_;
     }
 
-    function createLock(
-        IERC20 token,
-        address sender,
-        uint256 time
-    ) internal {
-        TokenTimelock lock = new TokenTimelock(token, sender, time);
-        Locks.push(lock);
+    /**
+     * @return the token being held.
+     */
+    function token() public view virtual returns (IERC20) {
+        return _token;
     }
 
-    // Attempts to release tokens. This is done safely with
-    // OpenZeppelin which checks the proper time has passed.
-    // To see their code go to:
-    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/TokenTimelock.sol
-    function release(uint256 lock) external {
-        Locks[lock].release();
-        emit Release(lock, Locks[lock].beneficiary());
+    /**
+     * @return the beneficiary of the tokens.
+     */
+    function beneficiary() public view virtual returns (address) {
+        return _beneficiary;
     }
 
-    function getLockAddress(uint256 lock) external view returns (address) {
-        require(lock <= 8, "getLockAddress: lock doesnt exist");
-        return address(Locks[lock]);
+    /**
+     * @return the time when the tokens are released.
+     */
+    function releaseTime() public view virtual returns (uint256) {
+        return _releaseTime;
     }
 
+    /**
+     * @notice Transfers tokens held by timelock to beneficiary.
+     */
+    function release() public virtual {
+        require(block.timestamp >= releaseTime(), "WigoVesting: current time is before release time");
+
+        uint256 amount = token().balanceOf(address(this));
+        require(amount > 0, "WigoVesting: no tokens to release");
+
+        token().safeTransfer(beneficiary(), amount);
+    }
 }
